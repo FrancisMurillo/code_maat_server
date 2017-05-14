292e38c9f6fb922dbfadfc5507c262f9bdfe6072
Francis Murillo <francisavmurillo@gmail.com>
2017-05-14 20:25:49 +0800

    Entity chrun implmented

--- app.py
+++ app.py
@@ -67,6 +67,9 @@ def log_file():
     elif analysis == 'author-churn':
         return jsonify(
             read_csv_file(cli.generate_author_churn_file(log_file)))
+    elif analysis == 'entity-churn':
+        return jsonify(
+            read_csv_file(cli.generate_entity_churn_file(log_file)))
     else:
         return ("ERROR: Analysis type not in selection.", 400)
 
